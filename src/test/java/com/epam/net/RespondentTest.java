6fe7662222ea57db258f8825d74a813263c7e2e9
SovietSir <sinnxronn@yandex.ru>
2017-09-11 18:11:36 +0300

    #3 - Add tests for getResponce method

--- src/test/java/com/epam/net/RespondentTest.java
+++ src/test/java/com/epam/net/RespondentTest.java
@@ -1,14 +1,51 @@
 package com.epam.net;
 
-import io.vavr.Tuple2;
-import org.junit.jupiter.api.Test;
+import com.epam.daoInterfaces.LeagueDAO;
+import com.epam.model.League;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
+import io.vavr.Tuple2;
+import org.testng.annotations.*;
+
+import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.testng.Assert.*;
 
 class RespondentTest {
-    private static Respondent respondent = new Respondent();
+    private Respondent respondent;
+    private Gson gson;
+    private ArrayList <League> leaguesList;
+
+    @BeforeClass
+    void setup() {
+        respondent = new Respondent();
+        gson = new GsonBuilder().create();
+        leaguesList = new ArrayList<>();
+
+        leaguesList.add(new League(1,"RFPL"));
+        leaguesList.add(new League(2,"APL"));
+        leaguesList.add(new League(3,"PDL"));
+        leaguesList.add(new League(4,"USPL"));
+
+        respondent.setLeagueDAO(new LeagueDAO() {
+            @Override
+            public void create(League league) {}
+            @Override
+            public League readById(Long aLong) {
+                return leaguesList.get(aLong.intValue());
+            }
+            @Override
+            public void update(Long aLong, League league) {}
+            @Override
+            public void deleteById(Long aLong) {}
+            @Override
+            public List<League> readAll() {
+                return leaguesList;
+            }
+        });
+    }
 
     @Test
     void parseCorrect() {
@@ -21,8 +58,29 @@ class RespondentTest {
 
     @Test
     void parseIncorrect() {
+        assertNull(respondent.parse("leagues/10/events"));
         assertNull(respondent.parse("/leagues/events"));
         assertNull(respondent.parse("/leagues/102f/events"));
         assertNull(respondent.parse("/leaGues/10/events"));
+    }
+
+    @Test (dataProvider = "Request-Responce provider")
+    public void testGetResponse(String request, String responce) throws Exception {
+        assertEquals(respondent.getResponse(request), responce);
+        //System.out.println("Request is: " + request + "Expected responce is" + responce);
+    }
+
+    @DataProvider (name = "Request-Responce provider")
+    public Object[][] parseLocaleData() {
+        return new Object[][]{
+                // Non-implemented (or unknown) method
+                {"HEAD / HTTP/1.1", "HTTP/1.1 501 Not Implemented\r\n\r\n"},
+                // Bad URL
+                {"GET /// HTTP/1.1", "HTTP/1.1 400 Bad Request\r\n\r\n"},
+                // GET method
+                {"GET / HTTP/1.1", "HTTP/1.1 200 OK\r\nContent-Type: application/json\r\n\r\n{\"content\": \"start page\"}"},
+
+                {"GET /leagues HTTP/1.1", "HTTP/1.1 200 OK\r\nContent-Type: application/json\r\n\r\n" + gson.toJson(leaguesList)},
+        };
     }
 }
