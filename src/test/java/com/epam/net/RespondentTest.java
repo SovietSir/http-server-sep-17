852fc0d1bb27a54342eafcb8eb2064aebbf35c56
SovietSir <sinnxronn@yandex.ru>
2017-09-13 14:37:26 +0300

    #3 - Add tests for GET method
    Added cases on route /event

--- src/test/java/com/epam/net/RespondentTest.java
+++ src/test/java/com/epam/net/RespondentTest.java
@@ -1,9 +1,7 @@
 package com.epam.net;
 
-import com.epam.daoInterfaces.EventDAO;
-import com.epam.daoInterfaces.LeagueDAO;
-import com.epam.model.Event;
-import com.epam.model.League;
+import com.epam.daoInterfaces.*;
+import com.epam.model.*;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
@@ -21,6 +19,7 @@ class RespondentTest {
     private Gson gson;
     private ArrayList<League> leaguesList;
     private ArrayList<Event> eventsList;
+    private ArrayList<Offer> offersList;
     private Map<String, String> defaultHeaders;
 
     @BeforeClass
@@ -31,6 +30,7 @@ class RespondentTest {
                 .create();
         leaguesList = new ArrayList<>();
         eventsList = new ArrayList<>();
+        offersList = new ArrayList<>();
         defaultHeaders = new HashMap<String, String>();
         defaultHeaders.put("Content-Type", "application/json");
 
@@ -44,6 +44,11 @@ class RespondentTest {
         eventsList.add(new Event(2, 1, from(Instant.parse("2008-11-04T11:20:35.00Z")), "Andji", "Tom", "1:3"));
         eventsList.add(new Event(3, 1, from(Instant.parse("2009-10-05T12:25:40.00Z")), "Bavaria", "MU", "0:0"));
         eventsList.add(new Event(4, 1, from(Instant.parse("2010-09-06T13:30:45.00Z")), "Dinamo", "Sokol", "3:2"));
+
+        offersList.add(new Offer(1, 1, "Zenith will win Nadir", 0.95f, true));
+        offersList.add(new Offer(2, 2, "Andji will lose Tom", 0.65f, true));
+        offersList.add(new Offer(3, 3, "Bavaria had to destroy MU", 0.999f, true));
+        offersList.add(new Offer(4, 4, "Dinamo will split with Sokol", 0.5f, true));
 
         respondent.setLeagueDAO(new LeagueDAO() {
             @Override
@@ -98,10 +103,37 @@ class RespondentTest {
             }
         });
 
+        respondent.setOfferDAO(new OfferDAO() {
+
+            @Override
+            public void create(Offer offer) {}
+
+            @Override
+            public Offer readById(Long aLong) {
+                return offersList.get(aLong.intValue());
+            }
+
+            @Override
+            public void update(Long aLong, Offer offer) {
+
+            }
+
+            @Override
+            public void deleteById(Long aLong) {
+
+            }
+
+            @Override
+            public List<Offer> readOffersByEventId(long eventId) {
+                int intId = (int) eventId;
+                return offersList.subList(intId, intId + 1);
+            }
+        });
+
 
     }
 
-    @Test(groups = {"net"})
+    @Test()
     void parseCorrect() {
         List<Tuple2<String, Long>> list = respondent.parse("/leagues/10/events");
         assertEquals("leagues", list.get(0)._1);
@@ -110,7 +142,7 @@ class RespondentTest {
         assertNull(list.get(1)._2);
     }
 
-    @Test(groups = {"net"})
+    @Test()
     void parseIncorrect() {
         assertNull(respondent.parse("leagues/10/events"));
         assertNull(respondent.parse("/leagues/events"));
@@ -118,7 +150,7 @@ class RespondentTest {
         assertNull(respondent.parse("/leaGues/10/events"));
     }
 
-    @Test(dataProvider = "Request-Responce provider", groups = {"net"})
+    @Test(dataProvider = "Request-Responce provider")
     public void testGetResponse(String request, HttpResponse responce) throws Exception {
         assertEquals(respondent.getResponse(request), responce);
         //System.out.println("Request is: " + request + "Expected responce is" + responce);
@@ -131,17 +163,25 @@ class RespondentTest {
                 {"HEAD / HTTP/1.1\r\n", new HttpResponse(HttpCodes.NOT_IMPLEMENTED, null, null)},
                 // Bad URL
 //                {"GET /// HTTP/1.1\r\n", "HTTP/1.1 400 Bad Request\r\n\r\n"},
+                // Another bad URL
+                {"GET * HTTP/1.1\r\n", new HttpResponse(HttpCodes.BAD_REQUEST, null, null)},
+
                 // GET method
+                // startPage
                 {"GET / HTTP/1.1\r\n", new HttpResponse(HttpCodes.OK, defaultHeaders, "{\"content\": \"start page\"}")},
-//
-//                {"GET /leagues HTTP/1.1\r\nbody", "HTTP/1.1 200 OK\r\nContent-Type: application/json\r\n\r\n" + gson.toJson(leaguesList)},
+                // leagues
                 {"GET /leagues HTTP/1.1\r\nbody", new HttpResponse(HttpCodes.OK, defaultHeaders, gson.toJson(leaguesList))},
 
-//                {"GET /leagues/1 HTTP/1.1\r\nbody", "HTTP/1.1 200 OK\r\nContent-Type: application/json\r\n\r\n" + gson.toJson(leaguesList.get(1
                 {"GET /leagues/1 HTTP/1.1\r\nbody", new HttpResponse(HttpCodes.OK, defaultHeaders, gson.toJson(leaguesList.get(1)))},
 
-//                {"GET /leagues/1/events HTTP/1.1\r\nbody", "HTTP/1.1 200 OK\r\nContent-Type: application/json\r\n\r\n" + gson.toJson(leaguesList.get(1))},
-                {"GET /leagues/1/events HTTP/1.1\r\nbody", new HttpResponse(HttpCodes.OK, defaultHeaders, gson.toJson(eventsList))}
+                {"GET /leagues/1/events HTTP/1.1\r\nbody", new HttpResponse(HttpCodes.OK, defaultHeaders, gson.toJson(eventsList))},
+
+                // events
+                {"GET /events HTTP/1.1\r\nbody", new HttpResponse(HttpCodes.OK, defaultHeaders, gson.toJson(eventsList))},
+
+                {"GET /events/3 HTTP/1.1\r\nbody", new HttpResponse(HttpCodes.OK, defaultHeaders, gson.toJson(eventsList.get(3)))},
+
+                {"GET /events/3/offers HTTP/1.1\r\nbody", new HttpResponse(HttpCodes.OK, defaultHeaders, gson.toJson(offersList.subList(3, 4)))},
         };
     }
 }
