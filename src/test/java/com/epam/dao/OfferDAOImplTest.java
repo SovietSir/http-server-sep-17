f177619ebd3bd6f9e3656754e6015fac880789a5
Mikhail Sosnin <mikhail_sosnin@epam.com>
2017-09-14 14:05:54 +0300

    #3_SPIKE_testing_approaches Add OfferDAOImplTest

---
+++ src/test/java/com/epam/dao/OfferDAOImplTest.java
@@ -0,0 +1,28 @@
+package com.epam.dao;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class OfferDAOImplTest {
+    @Test
+    public void testReadOffersByEventId() throws Exception {
+    }
+
+    @Test
+    public void testCreate() throws Exception {
+    }
+
+    @Test
+    public void testReadById() throws Exception {
+    }
+
+    @Test
+    public void testUpdate() throws Exception {
+    }
+
+    @Test
+    public void testDeleteById() throws Exception {
+    }
+
+}
