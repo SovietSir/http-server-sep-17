b2bb007381f543f0e7ead6d60b7ce63ebf848ef9
Mikhail Sosnin <mikhail_sosnin@epam.com>
2017-09-14 14:03:19 +0300

    #3_SPIKE_testing_approaches Add BetDaoImplTest

---
+++ src/test/java/com/epam/dao/BetDAOImplTest.java
@@ -0,0 +1,44 @@
+package com.epam.dao;
+
+import com.epam.model.League;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+
+import static org.testng.Assert.*;
+
+public class BetDAOImplTest {
+
+    private BetDAOImpl betDAO;
+    private ArrayList<League> betList;
+
+    @BeforeClass
+    void setup(){
+
+    }
+    @Test
+    public void testReadBetsByOfferId() throws Exception {
+    }
+
+    @Test
+    public void testReadBetsByPersonId() throws Exception {
+    }
+
+    @Test
+    public void testCreate() throws Exception {
+    }
+
+    @Test
+    public void testReadById() throws Exception {
+    }
+
+    @Test
+    public void testUpdate() throws Exception {
+    }
+
+    @Test
+    public void testDeleteById() throws Exception {
+    }
+
+}
