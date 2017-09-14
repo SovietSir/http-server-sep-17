4b50710e206db853aed40cd8e31cc60a230095fd
Mikhail Sosnin <mikhail_sosnin@epam.com>
2017-09-14 14:06:11 +0300

    #3_SPIKE_testing_approaches Add PersonDAOImplTest

---
+++ src/test/java/com/epam/dao/PersonDAOImplTest.java
@@ -0,0 +1,28 @@
+package com.epam.dao;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class PersonDAOImplTest {
+    @Test
+    public void testReadAll() throws Exception {
+    }
+
+    @Test
+    public void testCreate() throws Exception {
+    }
+
+    @Test
+    public void testReadById() throws Exception {
+    }
+
+    @Test
+    public void testUpdate() throws Exception {
+    }
+
+    @Test
+    public void testDeleteById() throws Exception {
+    }
+
+}
