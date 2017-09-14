c0c8c879e746d4b11493a6ed23131ab02e23af39
Mikhail Sosnin <mikhail_sosnin@epam.com>
2017-09-14 14:05:39 +0300

    #3_SPIKE_testing_approaches Add EventDAOImplTest

---
+++ src/test/java/com/epam/dao/EventDAOImplTest.java
@@ -0,0 +1,32 @@
+package com.epam.dao;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class EventDAOImplTest {
+    @Test
+    public void testReadAll() throws Exception {
+    }
+
+    @Test
+    public void testReadEventsByLeagueId() throws Exception {
+    }
+
+    @Test
+    public void testCreate() throws Exception {
+    }
+
+    @Test
+    public void testReadById() throws Exception {
+    }
+
+    @Test
+    public void testUpdate() throws Exception {
+    }
+
+    @Test
+    public void testDeleteById() throws Exception {
+    }
+
+}
