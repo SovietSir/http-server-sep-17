3276fb29addf47e26a144962ff56715e7ee1a7ec
Mikhail Sosnin <mikhail_sosnin@epam.com>
2017-09-14 14:39:02 +0300

    #3_SPIKE_testing_approaches Add EqualsAndHachCode to Offer

--- src/main/java/com/epam/model/Offer.java
+++ src/main/java/com/epam/model/Offer.java
@@ -1,10 +1,12 @@
 package com.epam.model;
 
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
+@EqualsAndHashCode
 public class Offer {
     private long id;
     private long eventId;
