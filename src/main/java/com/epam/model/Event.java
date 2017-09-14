f441d25016fcf876f5789791005ca9124d14eb60
Mikhail Sosnin <mikhail_sosnin@epam.com>
2017-09-14 15:42:58 +0300

    #3_SPIKE_testing_approaches Add EqualsAndHashCode to Event

--- src/main/java/com/epam/model/Event.java
+++ src/main/java/com/epam/model/Event.java
@@ -1,12 +1,14 @@
 package com.epam.model;
 
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
 
 @Getter
 @AllArgsConstructor
+@EqualsAndHashCode
 public class Event {
     private long id;
     private long leagueId;
