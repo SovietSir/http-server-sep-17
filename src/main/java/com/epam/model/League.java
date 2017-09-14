f812e2099f4aa84b4f4dfa7b77ef5db4b2c128ce
Mikhail Sosnin <mikhail_sosnin@epam.com>
2017-09-14 13:29:37 +0300

    #3_SPIKE_testing_approaches Add equals and hashcode methods to League

--- src/main/java/com/epam/model/League.java
+++ src/main/java/com/epam/model/League.java
@@ -1,10 +1,12 @@
 package com.epam.model;
 
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
+@EqualsAndHashCode
 public class League {
     private long id;
     private String name;
