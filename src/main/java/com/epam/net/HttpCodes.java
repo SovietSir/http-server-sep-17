2fa6e08ba80f9b869dfb84533b171ecffa66a55b
Mihail Poplavkov <Mikhail_Poplavkov@epam.com>
2017-09-15 19:29:53 +0300

    #13 Remove unused HTTP codes

--- src/main/java/com/epam/net/HttpCodes.java
+++ src/main/java/com/epam/net/HttpCodes.java
@@ -3,9 +3,7 @@ package com.epam.net;
 public enum HttpCodes {
     OK(200),
     BAD_REQUEST(400),
-    UNAUTHORIZED(401),
     NOT_FOUND(404),
-    METHOD_NOT_ALLOWED(405),
     INTERNAL_SERVER_ERROR(500),
     NOT_IMPLEMENTED(501);
 
