a514f0a36da8ba34988a3944eb15961790254b75
Mikhail Sosnin <mikhail_sosnin@epam.com>
2017-09-15 18:38:46 +0300

    #3_SPIKE_testing_approaches refactor and fix BetDAOImplTest

--- src/test/java/com/epam/dao/BetDAOImplTest.java
+++ src/test/java/com/epam/dao/BetDAOImplTest.java
@@ -10,7 +10,8 @@ import java.util.ArrayList;
 import java.util.NoSuchElementException;
 
 import static java.time.LocalDateTime.parse;
-import static org.testng.Assert.*;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNull;
 
 public class BetDAOImplTest {
 
@@ -49,14 +50,14 @@ public class BetDAOImplTest {
         leaguesList.add(new League(3, "PDL"));
         leaguesList.add(new League(4, "USPL"));
 
-        eventList.add(new Event(1, 1,  parse("2007-12-03T15:15:30"), "Zenith", "Nadir", "0:2"));
-        eventList.add(new Event(2, 2,  parse("2007-12-03T15:15:30"), "Andji", "Tom", "1:3"));
-        eventList.add(new Event(3, 3,  parse("2007-12-03T15:15:30"), "Bavaria", "MU", "0:0"));
-        eventList.add(new Event(4, 4,  parse("2007-12-03T15:15:30"), "Dinamo", "Sokol", "3:2"));
+        eventList.add(new Event(1, 1, parse("2007-12-03T15:15:30"), "Zenith", "Nadir", "0:2"));
+        eventList.add(new Event(2, 2, parse("2007-12-03T15:15:30"), "Andji", "Tom", "1:3"));
+        eventList.add(new Event(3, 3, parse("2007-12-03T15:15:30"), "Bavaria", "MU", "0:0"));
+        eventList.add(new Event(4, 4, parse("2007-12-03T15:15:30"), "Dinamo", "Sokol", "3:2"));
 
-        offerList.add(new Offer(1,1,"111",111,true));
-        offerList.add(new Offer(2,2,"222",222,false));
-        offerList.add(new Offer(3,3,"333",333,true));
+        offerList.add(new Offer(1, 1, "111", 111, true));
+        offerList.add(new Offer(2, 2, "222", 222, false));
+        offerList.add(new Offer(3, 3, "333", 333, true));
 
         leagueDAO.create(leaguesList.get(0));
         leagueDAO.create(leaguesList.get(1));
@@ -71,19 +72,19 @@ public class BetDAOImplTest {
         offerDAO.create(offerList.get(1));
         offerDAO.create(offerList.get(2));
 
-        personList.add(new Person(1,"user1",111,111));
-        personList.add(new Person(2,"user2",222,222));
-        personList.add(new Person(3,"user3",333,333));
+        personList.add(new Person(1, "user1", 111, 111));
+        personList.add(new Person(2, "user2", 222, 222));
+        personList.add(new Person(3, "user3", 333, 333));
 
         personDAO.create(personList.get(0));
         personDAO.create(personList.get(1));
         personDAO.create(personList.get(2));
 
 
-        betList.add(new Bet(1,1,1,111,1));
-        betList.add(new Bet(2,2,2,222,2));
-        betList.add(new Bet(3,2,2,333,3));
-        betList.add(new Bet(4,3,3,444,4));
+        betList.add(new Bet(1, 1, 1, 111, 1));
+        betList.add(new Bet(2, 2, 2, 222, 2));
+        betList.add(new Bet(3, 2, 2, 333, 3));
+        betList.add(new Bet(4, 3, 3, 444, 4));
 
         betDAO.create(betList.get(0));
         betDAO.create(betList.get(1));
@@ -92,64 +93,52 @@ public class BetDAOImplTest {
 
     @Test
     public void testReadBetsByOfferId() throws Exception {
-        assertEquals(betDAO.readBetsByOfferId(1L).get(0),betList.get(0));
+        assertEquals(betDAO.readBetsByOfferId(1L).get(0), betList.get(0));
     }
 
     @Test
     public void testReadBetsByPersonId() throws Exception {
-        assertEquals(betDAO.readBetsByPersonId(1L).get(0),betList.get(0));
+        assertEquals(betDAO.readBetsByPersonId(1L).get(0), betList.get(0));
     }
 
     @Test
     public void testCreate() throws Exception {
         betDAO.create(betList.get(3));
-        assertEquals(betDAO.read(4L),betList.get(3));
+        assertEquals(betDAO.read(4L), betList.get(3));
     }
 
     @Test
     public void testReadById() throws Exception {
-        assertEquals(betDAO.read(1L),betList.get(0));
+        assertEquals(betDAO.read(1L), betList.get(0));
     }
 
     @Test
     public void testUpdate() throws Exception {
-        betList.set(1,new Bet(2,1,1,111,1));
-        betDAO.update(2L,betList.get(1));
-        assertEquals(betDAO.read(2L),betList.get(1));
+        betList.set(1, new Bet(2, 1, 1, 111, 1));
+        betDAO.update(2L, betList.get(1));
+        assertEquals(betDAO.read(2L), betList.get(1));
     }
 
-    @Test (expectedExceptions = { NoSuchElementException.class})
+    @Test(expectedExceptions = {NoSuchElementException.class})
     public void testUpdateWithNoSuchElementException() throws Exception {
-        betList.set(1,new Bet(2,1,1,111,1));
-        betDAO.update(200L,betList.get(1));
+        betList.set(1, new Bet(2, 1, 1, 111, 1));
+        betDAO.update(200L, betList.get(1));
     }
 
-    @Test (expectedExceptions = { NoSuchElementException.class})
-    public void testdeleteWithNoSuchElementException() throws Exception {
+    @Test(expectedExceptions = {NoSuchElementException.class})
+    public void testDeleteWithNoSuchElementException() throws Exception {
         betDAO.delete(4L);
         betList.remove(3);
         assertNull(betDAO.read(4L));
     }
 
-    @Test (expectedExceptions = {BadRequestException.class})
+    @Test(expectedExceptions = {BadRequestException.class})
     public void testReadAll() throws SQLException {
         betDAO.readAll();
     }
-    @Test (expectedExceptions = {BadRequestException.class})
+
+    @Test(expectedExceptions = {BadRequestException.class})
     public void testReadSubLevel() throws SQLException {
         betDAO.readSubLevel(1L);
-    }
-    @Test
-    public void testReadSubLevel2() throws SQLException {
-//        return OfferDAOImpl.OFFER_DAO.readOffersByEventId(id);
-//        System.out.println(eventDAO.readSubLevel(2L));
-//        assertEquals(eventDAO.readSubLevel(1L),);
-//        System.out.println(betDAO.readSubLevel(1L));
-//        System.out.println(eventDAO.readSubLevel(1L));
-
-
-//        System.out.println(personDAO.readSubLevel(1L));
-//        System.out.println(leagueDAO.readSubLevel(1L));
-//        System.out.println(offerDAO.readSubLevel(1L));
     }
 }
